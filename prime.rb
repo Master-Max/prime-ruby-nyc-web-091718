3345549a452b1e3d002708e6d73168a8c242664f
Max Petersen <mglxp5@gmail.com>
2018-09-02 21:16:39 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(n)
+  if n <= 1
+    return FALSE
+  end
+  
+  i = 2
+  while i < n
+    if n != i && n % i == 0
+      return FALSE
+    else
+      i += 1
+    end
+  end
+  return TRUE
+end
