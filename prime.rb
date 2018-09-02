7dcf4727dd5c55aafc3ba7407f11ab09b6e24432
Max Petersen <mglxp5@gmail.com>
2018-09-02 21:12:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,12 @@
 # Add  code here!
 def prime?(n)
-  if n > 1
+  if n < 1
     return FALSE
   end
   
+  ans = FALSE
+  
   for i in 1..n
     if n % i == 0
-      return FALSE
-    else
-      return TRUE
-    end
-  end
+      ans = FALSE
 end
